759f224c0447c8b616775073f8265c4f446e509e
rendai-0914 <2020m054@kuas.ac.jp>
2020-05-22 03:42:05 +0900

    Added LogActivity and intent

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bttracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,4 +19,24 @@ public class MainActivity extends AppCompatActivity {
         Intent toLog = new Intent(this, LogActivity.class);
         startActivity(toLog);
     }
+
+    public void goToNormalActivity(View view) {
+        Intent toNormal = new Intent(this, NormalActivity.class);
+        startActivity(toNormal);
+    }
+
+    public void goToMechanismActivity(View view) {
+        Intent toMechanism = new Intent(this, MechanismActivity.class);
+        startActivity(toMechanism);
+    }
+
+    public void Link2(View view) {
+
+    }
+
+    public void goToHandlingActivity(View view) {
+       Intent toHandling = new Intent(this, HandlingActivity.class);
+       startActivity(toHandling);
+    }
 }
+
